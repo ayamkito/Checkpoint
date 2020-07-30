9ac1c252f4c97ddc1f038090b26b5f1de437e1fc
akwotiga kizito <kakwotiga@yahoo.com>
2020-07-30 19:01:50 -0400

    completed

--- Variables/index.js
+++ Variables/index.js
@@ -27,6 +27,7 @@ ASSIGN the variable named myFirstVariable with the data type null
 */
 
 var myFirstVariable;
+myFirstVariable=null
 
 /*
 QUESTION 2
@@ -37,6 +38,7 @@ Assign it to a BOOLEAN data type
 */
 
 var mySecondVariable;
+mySecondVariable=true
 
 // YOUR CODE HERE
 
@@ -48,6 +50,7 @@ ASSIGN mythirdVar to equal to a string with the text ==>  hello world
 */
 
 var myThirdVar;
+myThirdVar="hello world"
 
 // YOUR CODE HERE
 
@@ -59,6 +62,7 @@ ASSIGN myFourthVar to equal to a string with the text ==> This Is Case Sensitive
 */
 
 var myFourthVar;
+myFourthVar="This Is Case Sensitive"
 
 // YOUR CODE HERE
 
@@ -71,6 +75,7 @@ ASSIGN myAge to your age, use the number data type
 */
 
 var myAge;
+myAge=23
 
 // YOUR CODE HERE
 
@@ -85,6 +90,7 @@ i.e. + - / *
 */
 
 var myAgeCalc = null;
+myAgeCalc= 10*2+5-2
 
 // YOUR CODE HERE
 
@@ -98,9 +104,10 @@ refer to lecutre about expressions
 
 */
 
-(1 + 5) * 4;
+// (1 + 5) * 4;
 
 var numExpression;
+numExpression=5
 
 // YOUR CODE HERE
 
